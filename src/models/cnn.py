2e4318e3d0b3eaede2f41ed33e8e37d71e593e76
yavuzff <yavuzff@outlook.com>
2026-02-20 02:14:18 +0000

    add simple cnn

---
+++ src/models/cnn.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleCNN(nn.Module):
+    """
+    Simple CNN architecture with 4 convolutional layers followed by a fully connected layer.
+    """
+    def __init__(self, num_classes=2):
+        super(SimpleCNN, self).__init__()
+
+        # input is 3 channels, output channels increase with each layer
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # linear head
+        self.fc_input_dim = 128 * 3 * 3
+
+        self.fc = nn.Linear(self.fc_input_dim, num_classes)
+
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
